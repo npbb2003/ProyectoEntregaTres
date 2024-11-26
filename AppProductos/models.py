b2ce3880818f0780586dc854e60d00891f14b554
Natalia <npbb2003@yahoo.com.ar>
2024-11-25 22:54:36 -0300

    Incorporo primer parte del  proyecto funcionando

---
+++ AppProductos/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class CopiaImpresa(models.Model):
+    superficie = models.CharField(max_length=40)
+    tamanio = models.IntegerField()
+    precio = models.IntegerField()
+
+
+class Fotolibro(models.Model):
+    tamanio = models.IntegerField()
+    cant_hojas = models.IntegerField()
+    precio = models.IntegerField()
+
+
+class SesionFotografica(models.Model):
+    nombre = models.CharField(max_length=40)
+    descripcion = models.CharField(max_length=100)
+    activo = models.BooleanField()
+    precio = models.IntegerField()
